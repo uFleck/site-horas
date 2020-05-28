96880599aa943cb02ea91e1c03e1b7192f51c76b
Bruno Koster <bruno10762@gmail.com>
2020-05-28 18:56:15 -0300

    update script

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ function carregar() {
     var hora = 19
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
-        img.src = 'manha.png'
+        img.src = 'imagens/manha.png'
         document.body.style.background = '#b6b86d'
     } else if (hora >= 12 && hora <= 18) {
-        img.src = 'tarde.png'
+        img.src = 'imagens/tarde.png'
         document.body.style.background = '#e17a17'
     } else {
-        img.src = 'noite.png'
+        img.src = 'imagens/noite.png'
         document.body.style.background = 'black'
     }
 }
